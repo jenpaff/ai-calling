4c2797b159bc4e1ce47bbb0e2f6e91c9f16ff277
Karina.Ahrer <Karina.Ahrer@accenture.com>
2017-04-21 10:37:43 +0200

    changes to action_page.py: main(), predictions

--- templates/action_page.py
+++ templates/action_page.py
@@ -10,13 +10,25 @@ searchterm = form.getvalue('skill')
 
 #Project and model configuration
 project_id = 'ai-calling'
-model_id = 'skills-identifier'
+model_id = 'skillspredictions'
+
+def main():
+	""" Simple logic: train and make prediction """
+	try:
+		make_prediction()
+	except HttpError as e: 
+		if e.resp.status == 404: #model does not exist
+			print("Model does not exist yet.")
+			#train_model()
+			#make_prediction()
+		else: #real error
+			print(e)
 
 def make_prediction():
 	""" Use trained model to generate a new prediction """
 
 	api = get_prediction_api()
-	
+
 	print("Fetching model.")
 
 	#model = api.trainedmodels().get(project=project_id, id=model_id).execute()
@@ -50,7 +62,8 @@ def make_prediction():
 	#stats = prediction.get('outputMulti')
 
 	#show results
-	#print("You are currently %s (class %s)." % (labels[label], label) ) 
+    print("Prediction is working") 
+    print(prediction)
 	#print(stats)
 	
 
@@ -73,3 +86,27 @@ def get_prediction_api(service_account=True):
 		'https://www.googleapis.com/auth/devstorage.read_only'
 	]
 	return get_api('prediction', scope, service_account)
+
+
+def get_api(api, scope, service_account=True):
+	""" Build API client based on oAuth2 authentication """
+	STORAGE = file.Storage('oAuth2.json') #local storage of oAuth tokens
+	credentials = STORAGE.get()
+	if credentials is None or credentials.invalid: #check if new oAuth flow is needed
+		if service_account: #server 2 server flow
+			credentials = ServiceAccountCredentials('service_account.json', scopes=scope)
+			STORAGE.put(credentials)
+		else: #normal oAuth2 flow
+			CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
+			FLOW = client.flow_from_clientsecrets(CLIENT_SECRETS, scope=scope)
+			PARSER = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter, parents=[tools.argparser])
+			FLAGS = PARSER.parse_args(sys.argv[1:])
+			credentials = tools.run_flow(FLOW, STORAGE, FLAGS)
+
+	#wrap http with credentials
+	http = credentials.authorize(httplib2.Http())
+	return discovery.build(api, "v1.6", http=http)
+
+
+if __name__ == '__main__':
+	main()
