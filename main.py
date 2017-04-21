a9b6b952e5d24b9f7033112b8e7b8028c41e5bfc
Karina.Ahrer <Karina.Ahrer@accenture.com>
2017-04-21 10:12:29 +0200

    solve mistake

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def homepage():
 
     # Return a Jinja2 HTML template and pass in image_entities as a parameter.
     # return render_template('homepage.html', image_entities=image_entities)
-	return render_template('main.html')
+	return render_template('main.html', image_entities=image_entities)
 
 
 @app.route('/upload_photo', methods=['GET', 'POST'])
